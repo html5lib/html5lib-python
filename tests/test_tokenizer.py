f9530a3689dac7d74a942786379856b56a1f7e7f
James Graham <james@hoppipolla.co.uk>
2006-12-07 18:34:48 +0000

    Update tests to new format (running tests requires simplejson)
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%4077

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -2,11 +2,12 @@ import sys
 import glob
 import StringIO
 
+import simplejson
+
 #Allow us to import the parent module
 sys.path.insert(0, "../")
 
 import tokenizer
-
 
 class TokenizerTestParser(object):
     def parse(self, stream, innerHTML=False):
@@ -21,29 +22,29 @@ class TokenizerTestParser(object):
         return self.outputTokens
 
     def processDoctype(self, name, error):
-        self.outputTokens.append(["DOCTYPE", name, error])
+        self.outputTokens.append([u"DOCTYPE", name, error])
 
     def processStartTag(self, name, attributes):
-        self.outputTokens.append(["StartTag", name, attributes])
+        self.outputTokens.append([u"StartTag", name, attributes])
 
     def processEndTag(self, name):
-        self.outputTokens.append(["EndTag", name])
+        self.outputTokens.append([u"EndTag", name])
 
     def processComment(self, data):
-        self.outputTokens.append(["Comment", data])
+        self.outputTokens.append([u"Comment", data])
 
     def processCharacter(self, data):
-        self.outputTokens.append(["Character", data])
+        self.outputTokens.append([u"Character", data])
         
     def processEOF(self):
         pass
 
     def parseError(self):
-        self.outputTokens.append("ParseError")
+        self.outputTokens.append(u"ParseError")
 
     def atheistParseError(self):
         """This error is not an error"""
-        self.outputTokens.append("AtheistParseError")
+        self.outputTokens.append(u"AtheistParseError")
 
 def loadTests(f):
     for i, line in enumerate(f):
@@ -51,13 +52,32 @@ def loadTests(f):
             testList = eval(line)
             yield i+1, tuple(testList)
 
-def testTokenizer():
-    parser = TokenizerTestParser()
+def test_tokenizer():
     for filename in glob.glob('tokenizer/*.test'):
-        for i, (input, output, description)  in loadTests(open(filename)):
-            tokens = parser.parse(StringIO.StringIO(input))
-            try:
-                assert tokens == output
-            except AssertionError:
-                print "Failed test on line %i, %s"%(i, description)
-                print "Got", tokens, "expected", output 
+        tests = simplejson.load(file(filename))
+        for test in tests['tests']:
+            yield runTokenizerTest, test['description'], test['input'], test['output']
+
+def runTokenizerTest(description, input, output):
+    #XXX - move this out into the setup function
+    parser = TokenizerTestParser()
+    tokens = parser.parse(StringIO.StringIO(input))
+    try:
+        assert tokens == output
+    except AssertionError:
+        print "Failed test %s"%(description,)
+        print "Got", tokens, "expected", output
+        return False
+    return True
+
+def main():
+    failed = 0
+    tests = 0
+    for func, desc, input, output in test_tokenizer():
+        tests += 1
+        passed = func(desc, input, output)
+        if not passed: failed +=1
+    print "Ran %i tests, failed %i"%(tests, failed)
+
+if __name__ == "__main__":
+    main()
