08d1e292e26a4c91eec8a59daf94f542488ec016
James Graham <james@hoppipolla.co.uk>
2006-12-12 19:51:22 +0000

    Somewhat-working test harness. Just runs from the command line at the moment (python test_parser.py). Not yet using unittest as I can't work out how to make the error reporting work nicely
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40138

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath(os.pardir))
 
 import parser
 
-def testParser(testString):
+def parseTestcase(testString):
     testString = testString.split("\n")
     try:
         assert testString[0] == "#data"
@@ -25,7 +25,7 @@ def testParser(testString):
         if line and line[0] != "#":
             if currentList is output:
                 assert line[0] == "|"
-                currentList.append(line[1:])
+                currentList.append(line[2:])
             else:
                 currentList.append(line)
         elif line == "#errors":
@@ -33,6 +33,17 @@ def testParser(testString):
         elif line == "#document":
             currentList = output
     return "\n".join(input), "\n".join(output), errors
+
+def convertTreeDump(treedump):
+    """convert the output of str(document) to the format used in the testcases"""
+    treedump = treedump.split("\n")
+    rv = []
+    for line in treedump:
+        if line.startswith("#document"):
+            pass
+        else:
+            rv.append(line[3:])
+    return "\n".join(rv)
 
 def test_parser():
     for filename in glob.glob('tree-construction/*.dat'):
@@ -45,36 +56,49 @@ def test_parser():
                 #Strip out newlinw characters from the end of the string
                 test.append(line[:-1])
             else:
-                input, output, errors = testParser("\n".join(test))
-                yield TestCase.runParserTest, input, output, errors
+                input, output, errors = parseTestcase("\n".join(test))
+                yield runParserTest, input, output, errors
                 test = []
             lastLine = line
 
-class TestCase(unittest.TestCase):
-    def runParserTest(self, input, output, errors):
-        #XXX - move this out into the setup function
-        #concatenate all consecutive character tokens into a single token
-        p = parser.HTMLParser()
-        document = p.parse(StringIO.StringIO(input))
-        try:
-            #Need a check on the number of parse errors here
-            print str(document)
-            self.assertTrue(output == str(document))
-        except AssertionError:
-            raise
+def runParserTest(input, output, errors):
+    #XXX - move this out into the setup function
+    #concatenate all consecutive character tokens into a single token
+    p = parser.HTMLParser()
+    document = p.parse(StringIO.StringIO(input))
+    try:
+        #Need a check on the number of parse errors here
+        assert output == convertTreeDump(document.printTree())
+    except AssertionError:
+        print "input"
+        print input
+        print "expected output"
+        print output
+        print "recieved"
+        print convertTreeDump(document.printTree())
+        print ""
+        raise
+
 def main():
     failed = 0
     tests = 0
     for func, input, output, errors in test_parser():
         tests += 1
         testName = 'test%d' % tests
-        def testFunc(self, method=func, input=input,
-                     output=output, errors=errors):
-            method(self, input, output, errors)
-        testFunc.__doc__ = "\t".join([str(input)]) 
-        instanceMethod = new.instancemethod(testFunc, None, TestCase)
-        setattr(TestCase, testName, instanceMethod)
-    unittest.main()
+        try:
+            runParserTest(input, output, errors)
+        except AssertionError:
+            failed += 1
+
+    print "Ran %i tests, failed %i"%(tests, failed)
+
+#         def testFunc(self, method=func, input=input,
+#                      output=output, errors=errors):
+#             method(self, input, output, errors)
+#         testFunc.__doc__ = "\t".join([str(input)]) 
+#         instanceMethod = new
+#        setattr(TestCase, testName, instanceMethod)
+#   unittest.main()
 
 if __name__ == "__main__":
     main()
