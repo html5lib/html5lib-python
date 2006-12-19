6e175c0744adf53048db808e2ced52b4f303b5ae
lantis63 <lantis63@gmail.com>
2006-12-19 23:23:07 +0000

    Style cleanup in inputstream
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40256

--- inputstream.py
+++ inputstream.py
@@ -1,39 +1,41 @@
-import codecs, re
+import codecs
+import re
 
 from constants import EOF
 
 class HTMLInputStream(object):
-    """ Provides a unicode stream of characters to the HTMLTokenizer.
+    """Provides a unicode stream of characters to the HTMLTokenizer.
 
     This class takes care of character encoding and removing or replacing
     incorrect byte-sequences and also provides column and line tracking.
+
     """
 
     def __init__(self, source, encoding=None):
-        """ Initialise the HTMLInputStream.
+        """Initialises the HTMLInputStream.
 
-        The source can either be a file-object, filename, url or string
+        HTMLInputStream(source, [encoding]) -> Normalized stream from source
+        for use by the HTML5Lib.
+
+        source can be either a file-object, local filename or a string.
 
         The optional encoding parameter must be a string that indicates
         the encoding.  If specified, that encoding will be used,
         regardless of any BOM or later declaration (such as in a meta
         element)
-        """
 
+        """
         # List of where new lines occur
         self.newLines = []
 
         # Encoding Information
         self.charEncoding = encoding
 
-        # Original Stream
+        # Raw Stream
         self.rawStream = self.openStream(source)
 
         # Try to detect the encoding of the stream by looking for a BOM
         detectedEncoding = self.detectEncoding()
-
-        # Note whether a BOM needs to be skipped over
-        self.skipBOM = detectedEncoding and 1 or 0
 
         # If an encoding was specified or detected from the BOM don't allow
         # the encoding to be changed futher into the stream
@@ -54,21 +56,20 @@ class HTMLInputStream(object):
         uString = re.sub('\r\n?', '\n', uString)
         uString = re.sub('\x00', '\xFFFD', uString)
 
-        uList = list(uString)
+        # Convert the unicode string into a list to be used as the data stream
+        self.dataStream = list(uString)
 
         # Reset position in the list to read from
         self.reset()
 
-        # Use the normalized unicode list as the data stream
-        self.dataStream = uList
-
     def openStream(self, source):
-        """ Opens source so it can be used as a file object
+        """Produces a file object from source.
 
-        Returns a file-like object.
+        source can be either a file object, local filename or a string.
+
         """
-        # Already a file-like object
-        if hasattr(source, 'seek'):
+        # Already a file object
+        if hasattr(source, 'read'):
             stream = source
         else:
             # Try opening from file system
@@ -76,18 +77,15 @@ class HTMLInputStream(object):
                 return open(source)
             except: pass
 
-            # Otherwise treat source as a string and covert to a file-like object
+            # Otherwise treat source as a string and convert to a file object
             import cStringIO
             stream = cStringIO.StringIO(str(source))
         return stream
 
     def detectEncoding(self):
-        """ Attempts to detect the character encoding of the stream.
-
-        If an encoding can be determined from the BOM return the name of the
-        encoding otherwise return None
-        """
-
+        # Attempts to detect the character encoding of the stream. If
+        # an encoding can be determined from the BOM return the name of the
+        # encoding otherwise return None
         bomDict = {
             codecs.BOM_UTF8: 'utf-8',
             codecs.BOM_UTF16_LE: 'utf-16-le', codecs.BOM_UTF16_BE: 'utf-16-be',
@@ -100,13 +98,17 @@ class HTMLInputStream(object):
 
         # Try detecting the BOM using bytes from the string
         encoding = bomDict.get(string[:3])       # UTF-8
+        seek = 3
         if not encoding:
             encoding = bomDict.get(string[:2])   # UTF-16
+            seek = 2
             if not encoding:
                 encoding = bomDict.get(string)   # UTF-32
+                seek = 4
 
-        # Go back to the beginning of the file
-        self.rawStream.seek(0)
+        # Set the read position past the BOM if one was found, otherwise
+        # set it to the start of the stream
+        self.rawStream.seek(encoding and seek or 0)
 
         return encoding
 
@@ -119,24 +121,24 @@ class HTMLInputStream(object):
         If the encoding is not compatible with the guessed encoding
         and non-US-ASCII characters have been seen, return True indicating
         parsing will have to begin again.
+
         """
         pass
 
     def determineNewLines(self):
-        """ Looks through the stream to find where new lines occur so
-        the position method can tell where it is.
-        """
-        self.newLines.append(self.skipBOM or 0)
+        # Looks through the stream to find where new lines occur so
+        # the position method can tell where it is.
+        self.newLines.append(0)
         for i in xrange(len(self.dataStream)):
             if self.dataStream[i] == u"\n":
                 self.newLines.append(i)
 
     def position(self):
-        """ Returns (line, col) position in the stream
-        """
-        # Calculate where new lines occur only if it hasn't been done yet
+        """Returns (line, col) of the current position in the stream."""
+        # Generate list of new lines first time around
         if not self.newLines:
             self.determineNewLines()
+
         line = 0
         tell = self.tell
         for pos in self.newLines:
@@ -144,20 +146,18 @@ class HTMLInputStream(object):
                 line += 1
             else:
                 break
-        col = tell-self.newLines[line-1]-1
+        col = tell - self.newLines[line-1] - 1
         return (line, col)
 
     def reset(self):
-        """ Resets the position in the stream back to the start
-        """
-        self.tell = self.skipBOM or 0
+        """Resets the position in the stream back to the start."""
+        self.tell = 0
 
     def readChar(self):
-        """ Read one character from the stream or EOF if EOF is reached.
-        """
+        """Read one character from the stream or EOF if EOF is reached."""
         try:
             self.tell += 1
-            return self.dataStream[self.tell-1]
+            return self.dataStream[self.tell - 1]
         except:
             return None
 
@@ -165,14 +165,14 @@ if __name__ == "__main__":
     try:
         stream = HTMLInputStream("tests/utf-8-bom.html")
 
-        c = stream.read(1)
+        c = stream.readChar()
         while c:
             line, col = stream.position()
             if c == u"\n":
                 print "Line %s, Column %s: Line Feed" % (line, col)
             else:
                 print "Line %s, Column %s: %s" % (line, col, c.encode('utf-8'))
-            c = stream.read(1)
+            c = stream.readChar()
         print "EOF"
     except IOError:
         print "The file does not exist."
