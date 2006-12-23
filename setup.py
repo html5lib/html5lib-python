a5cf93f01cd1a3ab33e680eaf8db807ba3c51307
James Graham <james@hoppipolla.co.uk>
2006-12-23 13:30:55 +0000

    First cut at a setup.py file. Not quite perfect; creates an unnecessary build directory and doesn't install tests
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40285

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='html5lib',
+      version='0.1',
+      url='http://code.google.com/p/html5lib/',
+      description='HTML parser based on the WHAT-WG Web Applications 1.0' 
+                  '("HTML5") specifcation',
+      packages=['html5lib'],
+      package_dir = {'html5lib': 'src'}
+      )
