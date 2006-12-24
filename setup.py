0a3b5f814f83a82a3b9ad76081a8b29e868c256a
James Graham <james@hoppipolla.co.uk>
2006-12-24 10:13:18 +0000

    Stage 1 in hacky move of trunk changes onto branch
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40301

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(name='html5lib',
       version='0.1',
       url='http://code.google.com/p/html5lib/',
+      license="MIT License",
       description='HTML parser based on the WHAT-WG Web Applications 1.0' 
                   '("HTML5") specifcation',
       packages=['html5lib'],
