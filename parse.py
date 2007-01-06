cad94e1ad8ca6d4ad25a45f4d1e348c12c08d351
James Graham <james@hoppipolla.co.uk>
2007-01-06 11:36:41 +0000

    Remove some garbage printed before the document
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40427

--- parse.py
+++ parse.py
@@ -69,7 +69,6 @@ def parse():
         print "\n\nRun took: %fs (plus %fs to print the output)"%(t1-t0, t2-t1)
     else:
         document = p.parse(f)
-        print document
         print p.tree.testSerializer(document)
         if opts.error:
             print "\nParse errors:\n" + "\n".join(p.errors)
