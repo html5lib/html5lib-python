a864209311b896a0a1c1a772d02073f42779af57
Sam Ruby <rubys@intertwingly.net>
2007-06-04 19:18:46 +0000

    More assertions
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40661

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -48,7 +48,10 @@ class HTMLInputStreamTest(unittest.TestCase):
     def test_newlines(self):
         stream = HTMLInputStream(codecs.BOM_UTF8 + "a\nbb\r\nccc\rdddd")
         self.assertEquals(stream.tell, 0)
-        self.assertEquals(stream.charsUntil('x'),u"a\nbb\nccc\ndddd")
+        self.assertEquals(stream.charsUntil('c'),u"a\nbb\n")
+        self.assertEquals(stream.tell, 6)
+        self.assertEquals(stream.position(), (3,1))
+        self.assertEquals(stream.charsUntil('x'),u"ccc\ndddd")
         self.assertEquals(stream.tell, 14)
         self.assertEquals(stream.position(), (4,5))
         self.assertEquals(stream.newLines, [0,1,4,8])
