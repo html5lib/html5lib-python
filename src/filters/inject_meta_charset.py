0dbe1542e9b55c507ffcc0939394c9dd4db278d2
Thomas Broyer <t.broyer@ltgt.net>
2007-06-20 14:29:11 +0000

    Added support for <meta http-equiv="Content-Type" content="..."> in inject_meta_charset filter (Python only) along with a few tests.
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40782

--- src/filters/inject_meta_charset.py
+++ src/filters/inject_meta_charset.py
@@ -19,10 +19,21 @@ class Filter(_base.Filter):
             elif type == "EmptyTag":
                 if token["name"].lower() == "meta":
                    # replace charset with actual encoding
+                   has_http_equiv_content_type = False
+                   content_index = -1
                    for i,(name,value) in enumerate(token["data"]):
-                       if name == 'charset':
-                          token["data"][i] = (token["data"][i][0], self.encoding)
+                       if name.lower() == 'charset':
+                          token["data"][i] = (u'charset', self.encoding)
                           meta_found = True
+                          break
+                       elif name == 'http-equiv' and value.lower() == 'content-type':
+                           has_http_equiv_content_type = True
+                       elif name == 'content':
+                           content_index = i
+                   else:
+                       if has_http_equiv_content_type and content_index >= 0:
+                           token["data"][content_index] = (u'content', u'text/html; charset=%s' % self.encoding)
+                           meta_found = True
 
                 elif token["name"].lower() == "head" and not meta_found:
                     # insert meta into empty head
