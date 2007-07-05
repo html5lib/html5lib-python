5fb13b7a32479d5b9692c3468d6adc82cc93892f
Anne van Kesteren <annevk@annevk.nl>
2007-07-05 14:57:25 +0000

    make tests5 work
    --HG--
    extra : convert_revision : svn%3Aacbfec75-9323-0410-a652-858a13e371e0/trunk%40860

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -88,7 +88,6 @@ def buildTestSuite():
     for treeName, treeCls in treeTypes.iteritems():
         for filename in html5lib_test_files('tree-construction'):
             testName = os.path.basename(filename).replace(".dat","")
-            if testName == "tests5": continue # TODO
 
             tests = TestData(filename, ("data", "errors", "document-fragment",
                                         "document"))
