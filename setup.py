2225181f7b2b60bbd40f62e1030a5e9bb55c5f8d
jgraham <james@hoppipolla.co.uk>
2010-01-17 11:59:12 +0100

    Some chnges to the metadata

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ except ImportError:
     from distutils.core import setup
 import os
 
-long_description="""HTML parser designed to follow the WHATWG HTML5 
+long_description="""HTML parser designed to follow the HTML5 
 specification. The parser is designed to handle all flavours of HTML and 
 parses invalid documents using well-defined error handling rules compatible
 with the behaviour of major desktop web browsers.
 
 Output is to a tree structure; the current release supports output to
-DOM, ElementTree, lxml and BeautifulSoup tree formats as well as a
+DOM, ElementTree and lxml tree formats as well as a
 simple custom format"""
 
 classifiers=[
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Stable',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
@@ -27,12 +27,11 @@ setup(name='html5lib',
       version='1.0',
       url='http://code.google.com/p/html5lib/',
       license="MIT License",
-      description='HTML parser based on the WHAT-WG Web Applications 1.0' 
-                  '("HTML5") specifcation',
+      description='HTML parser based on the HTML5 specifcation',
       long_description=long_description,
       classifiers=classifiers,
       maintainer='James Graham',
-      maintainer_email='jg307@cam.ac.uk',
+      maintainer_email='james@hoppipolla.co.uk',
       packages=['html5lib'] + ['html5lib.'+name
           for name in os.listdir(os.path.join('src','html5lib'))
           if os.path.isdir(os.path.join('src','html5lib',name)) and
