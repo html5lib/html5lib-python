3cc85c6b488f330b8935a7c2ecf9a4feb14fa68b
James Graham <jgraham@opera.com>
2010-06-17 18:32:18 +0200

    Make sure html5lib is on the path for tests

--- html5lib/tests/support.py
+++ html5lib/tests/support.py
@@ -2,10 +2,19 @@ import os
 import sys
 import glob
 
-import html5lib
+base_path = os.path.split(__file__)[0]
+try:
+    import html5lib
+except ImportError:
+    #development
+    sys.path.insert(0, os.path.abspath(os.path.join(base_path, 
+                                                    os.path.pardir,
+                                                    os.path.pardir)))
+    import html5lib
+
 from html5lib import html5parser, treebuilders
 
-base_path = os.path.split(__file__)[0]
+
 if os.path.exists(os.path.join(base_path, 'testdata')):
     #release
     test_dir = os.path.join(base_path, 'testdata')
