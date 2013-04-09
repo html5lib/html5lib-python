b1dfea6274d8de79753568e7f2a4e2a095ee778c
Geoffrey Sneddon <geoffers@gmail.com>
2013-04-09 16:53:41 +0100

    Get tests running again under git.

--- html5lib/tests/support.py
+++ html5lib/tests/support.py
@@ -7,20 +7,10 @@ import glob
 
 base_path = os.path.split(__file__)[0]
 
-if os.path.exists(os.path.join(base_path, 'testdata')):
-    #release
-    test_dir = os.path.join(base_path, 'testdata')
-else:
-    #development
-    test_dir = os.path.abspath(
-        os.path.join(base_path,
-                     os.path.pardir, os.path.pardir,
-                     os.path.pardir, 'testdata'))
-    assert os.path.exists(test_dir), "Test data not found"
-    #import the development html5lib
-    sys.path.insert(0, os.path.abspath(os.path.join(base_path, 
-                                                    os.path.pardir,
-                                                    os.path.pardir)))
+test_dir = os.path.join(base_path, 'testdata')
+sys.path.insert(0, os.path.abspath(os.path.join(base_path, 
+                                                os.path.pardir,
+                                                os.path.pardir)))
 
 import html5lib
 from html5lib import html5parser, treebuilders
