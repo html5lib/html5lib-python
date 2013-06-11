8b93b1853b53802a2ec8c0e39f4cdf7811e85760
Geoffrey Sneddon <geoffers@gmail.com>
2013-06-11 17:29:51 +0100

    Fix #57: flake8-run.sh should return non-zero on failure
    This adds __all__ to utils.py to explicitly export default_etree

--- flake8-run.sh
+++ flake8-run.sh
@@ -7,5 +7,8 @@ fi
 
 if [[ $FLAKE == "true" ]]; then
   find html5lib/ -name '*.py' -and -not -name 'constants.py' -print0 | xargs -0 flake8 --ignore=E501
+  flake1=$?
   flake8 --max-line-length=99 --ignore=E126 html5lib/constants.py
+  flake2=$?
+  exit $[$flake1 || $flake2]
 fi
