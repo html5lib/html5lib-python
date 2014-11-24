1025014f8011f013f2bf02d974da263d510cf54d
Geoffrey Sneddon <geoffers@gmail.com>
2014-11-24 01:49:47 +0000

    Our tokenizer currently never outputs adjacent Character tokens; expect this.

--- html5lib/tests/test_tokenizer.py
+++ html5lib/tests/test_tokenizer.py
@@ -182,7 +182,6 @@ def runTokenizerTest(test):
     parser = TokenizerTestParser(test['initialState'],
                                  test['lastStartTag'])
     tokens = parser.parse(test['input'])
-    tokens = concatenateCharacterTokens(tokens)
     received = normalizeTokens(tokens)
     errorMsg = "\n".join(["\n\nInitial state:",
                           test['initialState'],
