c2321b0234ce5b7555aa080446c872e81c6cb21a
Geoffrey Sneddon <geoffers@gmail.com>
2015-07-21 13:29:32 +0100

    Update packages even if they're installed on Travis already.

--- requirements-install.sh
+++ requirements-install.sh
@@ -5,14 +5,14 @@ if [[ $USE_OPTIONAL != "true" && $USE_OPTIONAL != "false" ]]; then
   exit 1
 fi
 
-pip install -r requirements-test.txt
+pip install -U -r requirements-test.txt
 
 if [[ $USE_OPTIONAL == "true" ]]; then
   if [[ $TRAVIS_PYTHON_VERSION == "pypy" || $TRAVIS_PYTHON_VERSION == "pypy3" ]]; then
-    pip install -r requirements-optional.txt
+    pip install -U -r requirements-optional.txt
   elif [[ $TRAVIS_PYTHON_VERSION == "2.6" ]]; then
-    pip install -r requirements-optional-2.6.txt
+    pip install -U -r requirements-optional-2.6.txt
   else
-    pip install -r requirements-optional-cpython.txt
+    pip install -U -r requirements-optional-cpython.txt
   fi
 fi
