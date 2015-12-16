22c2b1ac0fc9eb73aefde898f7b9c948e34dc041
Geoffrey Sneddon <geoffers@gmail.com>
2015-12-16 02:52:28 +0000

    Get rid of LintError and just use asserts
    All of these properties should always hold per the API, so asserts
    seem like a good match here.

--- html5lib/filters/lint.py
+++ html5lib/filters/lint.py
@@ -9,10 +9,6 @@ from ..constants import spaceCharacters
 spaceCharacters = "".join(spaceCharacters)
 
 
-class LintError(Exception):
-    pass
-
-
 class Filter(_base.Filter):
     def __iter__(self):
         open_elements = []
@@ -21,73 +17,56 @@ class Filter(_base.Filter):
             if type in ("StartTag", "EmptyTag"):
                 namespace = token["namespace"]
                 name = token["name"]
-                if namespace is not None and not isinstance(namespace, text_type):
-                    raise LintError("Tag namespace is not a string or None: %(name)r" % {"name": namespace})
-                if namespace == "":
-                    raise LintError("Empty tag namespace")
-                if not isinstance(name, text_type):
-                    raise LintError("Tag name is not a string: %(tag)r" % {"tag": name})
-                if not name:
-                    raise LintError("Empty tag name")
-                if type == "StartTag" and (not namespace or namespace == namespaces["html"]) and name in voidElements:
-                    raise LintError("Void element reported as StartTag token: %(tag)s" % {"tag": name})
-                elif type == "EmptyTag" and (not namespace or namespace == namespaces["html"]) and name not in voidElements:
-                    raise LintError("Non-void element reported as EmptyTag token: %(tag)s" % {"tag": token["name"]})
+                assert namespace is None or isinstance(namespace, text_type)
+                assert namespace != ""
+                assert isinstance(name, text_type)
+                assert name != ""
+                assert isinstance(token["data"], dict)
+                if (not namespace or namespace == namespaces["html"]) and name in voidElements:
+                    assert type == "EmptyTag"
+                else:
+                    assert type == "StartTag"
                 if type == "StartTag":
                     open_elements.append((namespace, name))
-                for (namespace, localname), value in token["data"].items():
-                    if namespace is not None and not isinstance(namespace, text_type):
-                        raise LintError("Attribute namespace is not a string or None: %(name)r" % {"name": namespace})
-                    if namespace == "":
-                        raise LintError("Empty attribute namespace")
-                    if not isinstance(localname, text_type):
-                        raise LintError("Attribute localname is not a string: %(name)r" % {"name": localname})
-                    if not localname:
-                        raise LintError("Empty attribute localname")
-                    if not isinstance(value, text_type):
-                        raise LintError("Attribute value is not a string: %(value)r" % {"value": value})
+                for (namespace, name), value in token["data"].items():
+                    assert namespace is None or isinstance(namespace, text_type)
+                    assert namespace != ""
+                    assert isinstance(name, text_type)
+                    assert name != ""
+                    assert isinstance(value, text_type)
 
             elif type == "EndTag":
                 namespace = token["namespace"]
                 name = token["name"]
-                if namespace is not None and not isinstance(namespace, text_type):
-                    raise LintError("Tag namespace is not a string or None: %(name)r" % {"name": namespace})
-                if namespace == "":
-                    raise LintError("Empty tag namespace")
-                if not isinstance(name, text_type):
-                    raise LintError("Tag name is not a string: %(tag)r" % {"tag": name})
-                if not name:
-                    raise LintError("Empty tag name")
+                assert namespace is None or isinstance(namespace, text_type)
+                assert namespace != ""
+                assert isinstance(name, text_type)
+                assert name != ""
                 if (not namespace or namespace == namespaces["html"]) and name in voidElements:
-                    raise LintError("Void element reported as EndTag token: %(tag)s" % {"tag": name})
-                start_name = open_elements.pop()
-                if start_name != (namespace, name):
-                    raise LintError("EndTag (%(end)s) does not match StartTag (%(start)s)" % {"end": name, "start": start_name})
+                    assert False, "Void element reported as EndTag token: %(tag)s" % {"tag": name}
+                else:
+                    start = open_elements.pop()
+                    assert start == (namespace, name)
 
             elif type == "Comment":
                 pass
 
             elif type in ("Characters", "SpaceCharacters"):
                 data = token["data"]
-                if not isinstance(data, text_type):
-                    raise LintError("Attribute name is not a string: %(name)r" % {"name": data})
-                if not data:
-                    raise LintError("%(type)s token with empty data" % {"type": type})
+                assert isinstance(data, text_type)
+                assert data != ""
                 if type == "SpaceCharacters":
-                    data = data.strip(spaceCharacters)
-                    if data:
-                        raise LintError("Non-space character(s) found in SpaceCharacters token: %(token)r" % {"token": data})
+                    assert data.strip(spaceCharacters) == ""
 
             elif type == "Doctype":
                 name = token["name"]
-                if name is not None and not isinstance(name, text_type):
-                    raise LintError("Tag name is not a string or None: %(tag)r" % {"tag": name})
+                assert name is None or isinstance(name, text_type)
                 # XXX: what to do with token["data"] ?
 
             elif type in ("ParseError", "SerializeError"):
                 pass
 
             else:
-                raise LintError("Unknown token type: %(type)s" % {"type": type})
+                assert False, "Unknown token type: %(type)s" % {"type": type}
 
             yield token
