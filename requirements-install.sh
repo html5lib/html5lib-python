7bfff4888905b2924bdd7599e530c6ee90965673
Geoffrey Sneddon <geoffers@gmail.com>
2017-02-19 23:19:26 +0000

    Test on min versions of all deps, not just six

--- requirements-install.sh
+++ requirements-install.sh
@@ -2,16 +2,16 @@
 
 pip install pip==6.1.0
 
+if [[ $DEP_VERSION == "min" ]]; then
+  sed -i'' -e 's/>=/==/g' requirements*.txt
+fi
+
 pip install -U -r requirements-test.txt
 
 if [[ $USE_OPTIONAL == "true" ]]; then
   pip install -U -r requirements-optional.txt
 fi
 
-if [[ $SIX_VERSION ]]; then
-  pip install six==$SIX_VERSION
-fi
-
 if [[ $CI == "true" ]]; then
-  pip install -U codecov
+  pip install codecov
 fi
