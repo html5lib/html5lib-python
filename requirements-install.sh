45863e2a004b247e67c421b99dfae4dcfee2ac4f
Geoffrey Sneddon <geoffers@gmail.com>
2017-02-21 00:50:47 +0000

    fixup! Test on min versions of all deps, not just six

--- requirements-install.sh
+++ requirements-install.sh
@@ -3,7 +3,9 @@
 if [[ $PIP_VERSION ]]; then
   pip install "pip==$PIP_VERSION"
 else
-  pip install -U pip setuptools wheel
+  # Upgrade pip first because the ancient Travis version is b0rked
+  pip install -U pip
+  pip install -U setuptools wheel
 fi
 
 if [[ $DEP_VERSION == "min" ]]; then
