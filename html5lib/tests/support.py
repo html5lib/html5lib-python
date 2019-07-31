49c765c2807ce99baaf91fa561307bb9e843a4c5
)_( <zborboa@google.com>
2019-07-31 12:18:32 -0700

    Fix typo

--- html5lib/tests/support.py
+++ html5lib/tests/support.py
@@ -147,7 +147,7 @@ convertExpected = convert(2)
 
 
 def errorMessage(input, expected, actual):
-    msg = ("Input:\n%s\nExpected:\n%s\nRecieved\n%s\n" %
+    msg = ("Input:\n%s\nExpected:\n%s\nReceived\n%s\n" %
            (repr(input), repr(expected), repr(actual)))
     if sys.version_info[0] == 2:
         msg = msg.encode("ascii", "backslashreplace")
