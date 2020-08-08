2c19b9899ab3a3e8bd0ca35e5d78544334204169
Miro Hrončok <miro@hroncok.cz>
2020-08-08 13:39:22 +0200

    Use Node.from_parent() constructor to support pytest 6
    Add a wrapper not to break pytest 4 (needed for Python 2 support).
    
        ============================= test session starts ==============================
        platform linux -- Python 3.9.0b5, pytest-6.0.1, py-1.9.0, pluggy-0.13.1
        rootdir: /builddir/build/BUILD/html5lib-1.1, configfile: pytest.ini
        plugins: expect-1.1.0
        collected 0 items / 1 error
    
        ==================================== ERRORS ====================================
        ________________________ ERROR collecting test session _________________________
        /usr/lib/python3.9/site-packages/pluggy/hooks.py:286: in __call__
            return self._hookexec(self, self.get_hookimpls(), kwargs)
        /usr/lib/python3.9/site-packages/pluggy/manager.py:93: in _hookexec
            return self._inner_hookexec(hook, methods, kwargs)
        /usr/lib/python3.9/site-packages/pluggy/manager.py:84: in <lambda>
            self._inner_hookexec = lambda hook, methods, kwargs: hook.multicall(
        html5lib/tests/conftest.py:105: in pytest_collect_file
            return TokenizerFile(path, parent)
        /usr/lib/python3.9/site-packages/_pytest/nodes.py:95: in __call__
            warnings.warn(NODE_USE_FROM_PARENT.format(name=self.__name__), stacklevel=2)
        E   pytest.PytestDeprecationWarning: Direct construction of TokenizerFile has been deprecated, please use TokenizerFile.from_parent.
        E   See https://docs.pytest.org/en/stable/deprecations.html#node-construction-changed-to-node-from-parent for more details.
    
    Fixes https://github.com/html5lib/html5lib-python/issues/505

--- html5lib/tests/sanitizer.py
+++ html5lib/tests/sanitizer.py
@@ -13,7 +13,7 @@ class SanitizerFile(pytest.File):
         with codecs.open(str(self.fspath), "r", encoding="utf-8") as fp:
             tests = json.load(fp)
         for i, test in enumerate(tests):
-            yield SanitizerTest(str(i), self, test=test)
+            yield SanitizerTest.from_parent(self, name=str(i), test=test)
 
 
 class SanitizerTest(pytest.Item):
