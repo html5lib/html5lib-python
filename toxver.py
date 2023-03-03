3c05d2dc9044d73615aae2957ef3de1552f6e0eb
Łukasz Langa <lukasz@langa.pl>
2023-03-03 13:51:53 +0100

    Document toxver.py

--- toxver.py
+++ toxver.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+
+"""
+usage: toxver.py [python-version] [deps]
+
+Returns a Tox environment name given a GHA matrix Python version and dependencies.
+Many GHA configurations do this with inline Bash scripts but we want our solution
+to be cross-platform and work on Windows workers, too.
+
+Examples:
+
+    $ toxver.py pypy-3.8 base
+    TOXENV=pypy3-base
+
+    $ toxver.py 2.7 oldest
+    TOXENV=py27-oldest
+
+    $ toxver.py ~3.12.0-0 optional
+    TOXENV=py312-optional
+
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -9,7 +30,7 @@ import sys
 
 def main(argv):
     if len(argv) != 3:
-        print("usage: toxver.py [python-version] [deps]", file=sys.stderr)
+        print(__doc__.strip(), file=sys.stderr)
         return 1
 
     deps = argv[2]
