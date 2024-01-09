d30e69ab1c0b01b7709750085bc1038fb2195d5e
Christian Clauss <cclauss@me.com>
2024-01-09 22:11:02 +0100

    Implement the test changes from #573

--- html5lib/tests/tokenizer.py
+++ html5lib/tests/tokenizer.py
@@ -246,7 +246,8 @@ class TokenizerTest(pytest.Item):
     def repr_failure(self, excinfo):
         traceback = excinfo.traceback
         ntraceback = traceback.cut(path=__file__)
-        excinfo.traceback = ntraceback.filter(excinfo)
+        filter_args = (excinfo,) if pytest.version_tuple >= (7, 4, 0) else ()
+        excinfo.traceback = ntraceback.filter(*filter_args)
 
         return excinfo.getrepr(funcargs=True,
                                showlocals=False,
