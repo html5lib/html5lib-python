462e6b3b91d0b02f15e79436b85bd46d18bd3592
Łukasz Langa <lukasz@langa.pl>
2024-01-10 16:39:13 +0100

    Use `ImportError` instead of bare `except:`

--- html5lib/tests/test_meta.py
+++ html5lib/tests/test_meta.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, unicode_literals
 import six
 try:
     from unittest.mock import Mock
-except:
+except ImportError:
     from mock import Mock
 
 from . import support
