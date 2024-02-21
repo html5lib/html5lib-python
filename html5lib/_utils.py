fd4f032bc090d44fb11a84b352dad7cbee0a4745
Łukasz Langa <lukasz@langa.pl>
2024-02-21 16:31:38 +0100

    Constant phases (#567)
    * Get rid of getPhases
    
    This added a fair bit of complexity, and notable made the Phase classes
    dynamically generated.
    
    However, by doing this, we no longer include "process the
    token using the rules for" phases in the debug log.
    
    Co-authored-by: Sam Sneddon <me@gsnedders.com>

--- html5lib/_utils.py
+++ html5lib/_utils.py
@@ -145,15 +145,3 @@ def moduleFactoryFactory(factory):
             return mod
 
     return moduleFactory
-
-
-def memoize(func):
-    cache = {}
-
-    def wrapped(*args, **kwargs):
-        key = (tuple(args), tuple(kwargs.items()))
-        if key not in cache:
-            cache[key] = func(*args, **kwargs)
-        return cache[key]
-
-    return wrapped
